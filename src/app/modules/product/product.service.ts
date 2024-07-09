227183a4af2295ac75c84bd755849168efc9732a
noyon-podder <noyon.podder7@gmail.com>
2024-07-10 01:44:21 +0600

    product service file completed

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,12 +1,54 @@
 import { TProduct } from './product.interface'
 import { Product } from './product.model'
 
+// create product
 const productCreateIntoDB = async (payload: TProduct) => {
   const result = await Product.create(payload)
 
   return result
 }
 
+// get all products
+const getAllProductsFromDB = async () => {
+  const result = await Product.find({ isDeleted: false })
+
+  return result
+}
+
+// get single all products
+const getSingleProductFromDB = async (id: string) => {
+  const result = await Product.findById({ _id: id, isDeleted: false })
+
+  return result
+}
+
+// update product
+const singleProductUpdateFromDB = async (
+  id: string,
+  payload: Partial<TProduct>,
+) => {
+  const result = await Product.findByIdAndUpdate({ _id: id }, payload, {
+    new: true,
+  })
+
+  return result
+}
+
+// delete product
+const deleteProductFromDB = async (id: string) => {
+  const result = await Product.findByIdAndUpdate(
+    { _id: id },
+    { isDeleted: true },
+    { new: true },
+  )
+
+  return result
+}
+
 export const ProductServices = {
   productCreateIntoDB,
+  getAllProductsFromDB,
+  getSingleProductFromDB,
+  singleProductUpdateFromDB,
+  deleteProductFromDB,
 }
